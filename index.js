355fb647dcff1b255838369cc40b7add23cd2569
RAQIB404 <raqibkhan2332@gmail.com>
2021-08-15 11:05:26 +0530

    DOM

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(document);
+console.log(document.title);
+document.title="Learning DOM!!";
+console.log(document.title);
+
+const heading = document.getElementById("hi");
+console.log(heading);
+
+const head = document.querySelectorAll("p");
+console.log(head);
